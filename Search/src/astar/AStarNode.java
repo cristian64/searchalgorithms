51b5aa6a98459367eb5734aaa77bdd1d596cbc8e
cristian64@gmail.com <cristian64@gmail.com@dffcbe74-c6ca-eaa3-3659-f136f0333a42>
2012-04-03 13:46:07 +0000

    Written some javadoc for the important methods.

--- Search/src/astar/AStarNode.java
+++ Search/src/astar/AStarNode.java
@@ -26,6 +26,13 @@ public class AStarNode implements Comparable<AStarNode>
     private boolean open;
     private boolean closed;
     
+    /**
+     * Node that is used for the algorithm AStar.
+     * @param x Component x of the position of the node. It represents the columns.
+     * @param y Component y of the position of the node. It represents the rows.
+     * @param cost Cost to step on this node.
+     * @param diagonalCost Cost to step on this node with a diagonal move.
+     */
     public AStarNode(int x, int y, int cost, int diagonalCost)
     {
         this.x = x;
@@ -41,6 +48,12 @@ public class AStarNode implements Comparable<AStarNode>
         closed = false;
     }
 
+    /**
+     * It is needed to be able to ordered the list of elements according to how promising they are.
+     * Determines which node is better according to the F value.
+     * @param o Another node that is going to be compared with.
+     * @return Returns 0 if they are the same. -1 if the first one is better. 1 in other situation.
+     */
     @Override
     public int compareTo(AStarNode o)
     {
@@ -52,6 +65,11 @@ public class AStarNode implements Comparable<AStarNode>
             return h - o.h;        
     }
     
+    /**
+     * Generates the adjacent nodes for this particular node.
+     * @param nodes Matrix of nodes to get to know how big the matrix is.
+     * @return Returns a list with the nodes surrounding this node.
+     */
     public ArrayList<AStarNode> generateChildren(AStarNode[][] nodes)
     {
         ArrayList<AStarNode> children = new ArrayList<AStarNode>();
@@ -97,6 +115,11 @@ public class AStarNode implements Comparable<AStarNode>
         return diagonalCost;
     }
     
+    /**
+     * Changes the parent node for the node.
+     * Afterwards, the G value is recalculated (and the F value as well).
+     * @param parentNode New parent node for the node.
+     */
     public void setParentNode(AStarNode parentNode)
     {
         this.parentNode = parentNode;
@@ -104,16 +127,29 @@ public class AStarNode implements Comparable<AStarNode>
         f = g + h;
     }
     
+    /**
+     * Checks, without changing the parent node, the possible G after changing it.
+     * @param parentNode Possible parent node for this node.
+     * @return The G value of the node if the parent was the one sent in the parameters.
+     */
     public int getPossibleG(AStarNode parentNode)
     {
         return parentNode.g + ((parentNode.x == x || parentNode.y == y) ? cost : diagonalCost);
     }
     
+    /**
+     * @return Indicates the node from where it came to this node.
+     */
     public AStarNode getParentNode()
     {
         return parentNode;
     }
     
+    /**
+     * Change the end node for this node.
+     * Afterwards, H is calculated.
+     * @param endNode End node (the goal).
+     */
     public void setEndNode(AStarNode endNode)
     {
         this.endNode = endNode;
@@ -130,6 +166,9 @@ public class AStarNode implements Comparable<AStarNode>
         f = g + h;
     }
     
+    /**
+     * @return Indicates the goal node.
+     */
     public AStarNode getEndNode()
     {
         return endNode;
@@ -150,21 +189,37 @@ public class AStarNode implements Comparable<AStarNode>
         return h;
     }
     
+    /**
+     * Checks if the node has been already add into the list of promising nodes.
+     * @return Return true if the node was already expanded.
+     */
     public boolean isOpen()
     {
         return open;
     }
     
+    /**
+     * Establish if the node was added to the list of promising nodes.
+     * @param value New value for the state of this variable.
+     */
     public void setOpen(boolean value)
     {
         open = value;
     }
     
+    /**
+     * Checks if the node has been already expanded.
+     * @return Return true if the node was already expanded.
+     */
     public boolean isClosed()
     {
         return closed;
     }
     
+    /**
+     * Establish if the node was expanded or node.
+     * @param value New value for the state of this variable.
+     */
     public void setClosed(boolean value)
     {
         closed = value;
